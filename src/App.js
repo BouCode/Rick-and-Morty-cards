6a4179af968b849aa891d9e5ea494c42d934f340
Joan Boulangger <joan.boulangger@gmail.com>
2020-04-04 11:00:26 -0500

    mejorando estilos

--- src/App.js
+++ src/App.js
@@ -4,30 +4,31 @@ import Cards from './components/card';
 import axios from 'axios';
 
 function App() {
-  const [posts, setPosts] = useState ({characters: []});
+	const [ posts, setPosts ] = useState({ characters: [] });
 
-  useEffect ( () => {
-    axios.get ('https://rickandmortyapi.com/api/character')
-    .then ( res => {
-      setPosts ({characters: res.data.results})
-      console.log (res)
-    })
-    .catch (
-      error => console.log (error)
-    )
-  }, [])
-  	return (<div>
-      <section className = "container my-4">
-        <div className = "row my-4">
-          {
-            posts.characters.map ( (character, index) => (
-              <Cards character ={character} key = {index} />
-            ))
-          }
-        </div>
-      </section>
-    </div>
-    );
+	useEffect(() => {
+		axios
+			.get('https://rickandmortyapi.com/api/character')
+			.then((res) => {
+				setPosts({ characters: res.data.results });
+				console.log(res);
+			})
+			.catch((error) => console.log(error));
+	}, []);
+	return (
+		<div>
+			<nav className="navbar navbar-dark bg-dark">
+				<div id="title" className="container">
+					<h1 className="text-white "> List of Rick and Morty characters </h1>
+				</div>
+			</nav>
+			<section className="container my-4">
+				<div className="row my-4">
+					{posts.characters.map((character, index) => <Cards character={character} key={index} />)}
+				</div>
+			</section>
+		</div>
+	);
 }
 
 export default App;
--- src/components/card.js
+++ src/components/card.js
@@ -1,13 +1,14 @@
 import React from 'react';
-
 function Cards({ character }) {
 	return (
 		<div className="col-md-4 my-3">
-			<div className="card">
+			<div className="card text-white bg-dark mb-3">
 				<img src={character.image} className="card-img-top" />
 				<div className="card-body">
-					<h5 className="card-title">Name: {character.name}</h5>
-					<ul id = "lista" >
+					<div id="title">
+						<h5 className="text-center">{character.name}</h5>
+					</div>
+					<ul id="lista">
 						<li>Specie: {character.species}</li>
 						<li>Gender: {character.gender}</li>
 					</ul>
